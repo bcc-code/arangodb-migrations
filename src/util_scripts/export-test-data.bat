26e8408b061fbeb245e7d204459a10fda7bd0224
Philip Dalen <philip.dalen@bcc.no>
2021-10-07 11:39:11 +0200

    updated windows scrips as well

--- src/util_scripts/export-test-data.bat
+++ src/util_scripts/export-test-data.bat
@@ -18,11 +18,11 @@ del /q %PATH_TO_TEST_DATA%\*
 
 arangodump  ^
   --server.database %DATABASE% ^
-  --server.username %USERNAME% ^
-  --server.password %PASSWORD% ^
-  --server.authentication "true" ^
+  --server.authentication "false" ^
   --server.endpoint %URL% ^
-  --output-directory %PATH_TO_TEST_DATA%
+  --output-directory %PATH_TO_TEST_DATA% ^
+  --include-system-collections "true" ^
+  --compress-output "false" 
 
 
 
